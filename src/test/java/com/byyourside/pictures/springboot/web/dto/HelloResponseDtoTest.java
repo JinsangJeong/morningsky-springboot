ca5c1cf87843305e462f8e0d2e4703832169c96f
JinsangJeong <nsdoogie@naver.com>
2020-10-01 21:28:14 +0900

    modified files

---
+++ src/test/java/com/byyourside/pictures/springboot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,21 @@
+package com.byyourside.pictures.springboot.web.dto;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void lombok_func_test() {
+        //given
+        String name = "test";
+        int amount = 100;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        //then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
