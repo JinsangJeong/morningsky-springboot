81e1ef15674d7de60730b0b5d7e3bb2af5d65f0e
JinsangJeong <nsdoogie@naver.com>
2020-10-02 13:54:00 +0900

    modified and added files

---
+++ src/main/java/com/byyourside/pictures/springboot/web/dto/PostsSaveRequestDto.java
@@ -0,0 +1,25 @@
+package com.byyourside.pictures.springboot.web.dto;
+
+import com.byyourside.pictures.springboot.domain.posts.Posts;
+import com.byyourside.pictures.springboot.service.posts.PostsService;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostsSaveRequestDto {
+    private String title;
+    private String content;
+    private String author;
+    @Builder
+    public PostsSaveRequestDto(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public Posts toEntity() {
+        return Posts.builder().title(title).content(content).author(author).build();
+    }
+}
