77290be2038d540a0d6c73ff279e7cdce81216e2
JinsangJeong <nsdoogie@naver.com>
2020-10-02 17:14:07 +0900

    게시글 수정, 삭제 기능 추가

--- src/main/java/com/byyourside/pictures/springboot/service/posts/PostsService.java
+++ src/main/java/com/byyourside/pictures/springboot/service/posts/PostsService.java
@@ -2,14 +2,17 @@ package com.byyourside.pictures.springboot.service.posts;
 
 import com.byyourside.pictures.springboot.domain.posts.Posts;
 import com.byyourside.pictures.springboot.domain.posts.PostsRepository;
+import com.byyourside.pictures.springboot.web.dto.PostsListResponseDto;
 import com.byyourside.pictures.springboot.web.dto.PostsResponseDto;
 import com.byyourside.pictures.springboot.web.dto.PostsSaveRequestDto;
 import com.byyourside.pictures.springboot.web.dto.PostsUpdateRequestDto;
 import javafx.geometry.Pos;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -31,11 +34,25 @@ public class PostsService {
         return id;
     }
 
-    @Transactional
+    @Transactional(readOnly = true)
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new
                 IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        postsRepository.delete(posts);
     }
 }
