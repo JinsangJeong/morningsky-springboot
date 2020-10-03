0264c8bc76a63231a2aa5dc7209e50c25eb4550c
JinsangJeong <nsdoogie@naver.com>
2020-10-03 15:10:55 +0900

    add modify OAuth files

--- src/main/java/com/byyourside/pictures/springboot/web/IndexController.java
+++ src/main/java/com/byyourside/pictures/springboot/web/IndexController.java
@@ -1,23 +1,32 @@
 package com.byyourside.pictures.springboot.web;
 
+import com.byyourside.pictures.springboot.config.auth.LoginUser;
+import com.byyourside.pictures.springboot.config.auth.dto.SessionUser;
 import com.byyourside.pictures.springboot.service.posts.PostsService;
 import com.byyourside.pictures.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+//import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    //private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-
+        //SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
