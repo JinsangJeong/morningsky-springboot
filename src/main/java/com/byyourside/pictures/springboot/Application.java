0b30dd16ede3debffc3e60b5a6c865f63cd3ccfd
JinsangJeong <nsdoogie@naver.com>
2020-10-03 18:59:24 +0900

    modified oauth with TEST files

--- src/main/java/com/byyourside/pictures/springboot/Application.java
+++ src/main/java/com/byyourside/pictures/springboot/Application.java
@@ -2,9 +2,9 @@ package com.byyourside.pictures.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+//import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
+//@EnableJpaAuditing  삭제됨 -> 분리하여 JpaConfig에서 만듬.
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
--- src/test/java/com/byyourside/pictures/springboot/web/HelloControllerTest.java
+++ src/test/java/com/byyourside/pictures/springboot/web/HelloControllerTest.java
@@ -1,12 +1,15 @@
 package com.byyourside.pictures.springboot.web;
 
+import com.byyourside.pictures.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -15,14 +18,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class, excludeFilters ={@ComponentScan.Filter
+        (type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)})
 
 public class HelloControllerTest {
     @Autowired
     private MockMvc mvc;
 
     @Test
-    public void reutrn_hello() throws Exception {
+    @WithMockUser(roles = "USER")
+    public void return_hello() throws Exception {
         String hello = "hello";
 
         mvc.perform(get("/hello"))
@@ -31,6 +36,7 @@ public class HelloControllerTest {
     }
 
     @Test
+    @WithMockUser(roles = "USER")
     public void return_helloDto() throws Exception {
         String name = "hello";
         int amount = 1000;
